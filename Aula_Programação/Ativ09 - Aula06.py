a989f4da5de7d6fee9c64ba0b8b1f2834fc1cc24
Felipe_Doart <flpd.araujo@outlook.com>
2025-03-26 20:41:55 -0300

    Atividade - Aula 06

---
+++ Aula_Programação/Ativ09 - Aula06.py
@@ -0,0 +1,9 @@
+
+a1 = float(input('Insira sua nota: '))
+a2 = float(input('Insura sua nota: '))
+
+med =  (a1 + a2) /2
+if  med >= 6.0:
+    print(f'Aluno aprovado, média {med}')
+else:
+    print(f'Aluno Reprovaod, média {med}')
