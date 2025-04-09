5fdd6aaf533533d1da23b463fefbccf5435cd941
Felipe_Doart <flpd.araujo@outlook.com>
2025-04-09 20:33:13 -0300

    Delete Aula_Programação directory

--- Aula_Programação/Ativ09 - Aula06.py
+++
@@ -1,9 +0,0 @@
-
-a1 = float(input('Insira sua nota: '))
-a2 = float(input('Insura sua nota: '))
-
-med =  (a1 + a2) /2
-if  med >= 6.0:
-    print(f'Aluno aprovado, média {med}')
-else:
-    print(f'Aluno Reprovaod, média {med}')
